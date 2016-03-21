7498697f572304ccf4d0d2af53a58c6b82c469dc
Joe Desbonnet <jdesbonnet@gmail.com>
2016-03-21 14:32:24 +0000

    add some comments

--- scripts/host-if-up.sh
+++ scripts/host-if-up.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Motivation: every time BBB boots I have a different IF name 
+# formatted as enxXXXXXXXXXX . So want to apply ifconfig to
+# setup network connection to BeagleBone Black using first
+# interface starting with 'enx'.
 usb_if=(`netstat -i | grep enx`)
 echo "BBB USB interface: $usb_if"
 ifconfig $usb_if 192.168.7.1 netmask 255.255.255.252
